6198b43862421632f71e7e0357058a3c14fc4bb8
veschembes <mveschembes@gmail.com>
2021-11-29 16:21:00 +0100

    Création de fichiers vides dans le projet afin de téléverser les dossiers Modele et Interface

---
+++ app/src/main/java/com/btssio/joel/application_gsb/Interface/Empty.java
@@ -0,0 +1,8 @@
+package com.btssio.joel.application_gsb.Interface;
+
+/**
+ * Created by veschembes on 29/11/2021.
+ */
+
+public class Empty {
+}
---
+++ app/src/main/java/com/btssio/joel/application_gsb/Modele/Empty.java
@@ -0,0 +1,8 @@
+package com.btssio.joel.application_gsb.Modele;
+
+/**
+ * Created by veschembes on 29/11/2021.
+ */
+
+public class Empty {
+}
