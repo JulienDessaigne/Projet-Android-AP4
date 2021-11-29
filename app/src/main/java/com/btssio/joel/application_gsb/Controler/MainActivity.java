430cd7aa12f98ebb510b70611874a42dece214e0
veschembes <mveschembes@gmail.com>
2021-11-29 16:15:16 +0100

    Déplacement de MainActivity.java dans /Controler

--- app/src/main/java/com/btssio/joel/application_gsb/Controler/MainActivity.java
+++ app/src/main/java/com/btssio/joel/application_gsb/Controler/MainActivity.java
@@ -1,7 +1,9 @@
-package com.btssio.joel.application_gsb;
+package com.btssio.joel.application_gsb.Controler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.btssio.joel.application_gsb.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
