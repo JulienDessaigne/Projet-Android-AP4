ae7dfb2360dfbf4a73a39aa4b630670cb7990fb8
warmee <mttwarmee@gmail.com>
2021-12-13 14:50:38 +0100

    Ajouts des fonction permettant la récuperation des médecins et départements

--- app/src/main/java/com/btssio/AP4G2/application_gsb/Modele/PraticienDAOConnecte.java
+++ app/src/main/java/com/btssio/AP4G2/application_gsb/Modele/PraticienDAOConnecte.java
@@ -1,18 +1,47 @@
 package com.btssio.AP4G2.application_gsb.Modele;
 
+import android.util.Log;
+
 import com.btssio.AP4G2.application_gsb.Interface.ApiClient;
 import com.btssio.AP4G2.application_gsb.Interface.EventAsync;
 import com.btssio.AP4G2.application_gsb.Interface.InterfacePraticien;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by warmee on 06/12/2021.
  */
 
-public abstract class PraticienDAOConnecte implements EventAsync<Medecin>{
-    private InterfacePraticien apiMedcin;
+public abstract class PraticienDAOConnecte implements EventAsync<Praticien>{
+    private InterfacePraticien apiPraticien;
 
     public PraticienDAOConnecte(){
-        apiMedcin = ApiClient.getClient().create(InterfacePraticien.class);
+        apiPraticien = ApiClient.getClient().create(InterfacePraticien.class);
     }
 
+    public void getPraticienDAOConnecte(){
+        Call<ArrayList<Praticien>> callPraticien;
+        callPraticien = apiPraticien.lesPraticiens();
+        callPraticien.enqueue(new Callback<ArrayList<Praticien>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Praticien>> call, Response<ArrayList<Praticien>> response) {
+                if (response.isSuccessful()) {
+                    onTacheTerminee(response.body());
+                } else {
+                    int responseString = response.code();
+                    onErreur("erreur : "+responseString);
+                    Log.d("log", "erreur : "+responseString);
+                }
+            }
+            @Override
+            public void onFailure(Call<ArrayList<Praticien>> call, Throwable t) {
+                onErreur("onFailurePra : "+t.getMessage());
+                Log.d("log", "erreur : "+t.getMessage());
+            }
+        });
+    }
 }
