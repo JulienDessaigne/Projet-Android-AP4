f47b8d2620efe040184325671a8f820b0e263f5c
veschembes <mveschembes@gmail.com>
2021-12-13 14:50:17 +0100

    Modification des libelles des méthodes et modification du paramètre en GET au lieu de POST

--- app/src/main/java/com/btssio/AP4G2/application_gsb/Interface/InterfacePraticien.java
+++ app/src/main/java/com/btssio/AP4G2/application_gsb/Interface/InterfacePraticien.java
@@ -5,6 +5,9 @@ import com.btssio.AP4G2.application_gsb.Modele.Praticien;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
@@ -16,9 +19,10 @@ public interface InterfacePraticien {
     @POST("lesmedcins.php")
     Call<ArrayList<Praticien>> lesPraticiens();
 
-    @POST("lesmedcins_par_depart.php")
-    Call<ArrayList<Praticien>> lesmedcinsParDepart();
+    @FormUrlEncoded
+    @GET("lesmedcins_par_depart.php")
+    Call<ArrayList<Praticien>> lesPraticiensParDepart(@Field("libelledepartement") String libelleDepartement);
 
     @POST("lesmedcins_par_nom.php")
-    Call<ArrayList<Praticien>> lesmedcins_par_nom();
+    Call<ArrayList<Praticien>> lesPraticiensParNom();
 }
