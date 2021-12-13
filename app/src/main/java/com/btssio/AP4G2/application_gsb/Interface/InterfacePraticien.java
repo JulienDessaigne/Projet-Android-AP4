6651d8567b741806dbd8ce76c5f9e3499f753378
veschembes <mveschembes@gmail.com>
2021-12-13 16:39:24 +0100

    Rajout de la méthode getPraticiensParDepartementDAOConnecte() et de @NonNull

--- app/src/main/java/com/btssio/AP4G2/application_gsb/Interface/InterfacePraticien.java
+++ app/src/main/java/com/btssio/AP4G2/application_gsb/Interface/InterfacePraticien.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by veschembes on 29/11/2021.
@@ -18,8 +18,8 @@ public interface InterfacePraticien {
     @POST("lesmedcins.php")
     Call<ArrayList<Praticien>> lesPraticiens();
 
-    @GET("lesmedcins_par_depart.php?libelledepartement={libelledepartement}")
-    Call<ArrayList<Praticien>> lesPraticiensParDepart(@Path("libelledepartement") String libelleDepartement);
+    @GET("lesmedcins_par_depart.php")
+    Call<ArrayList<Praticien>> lesPraticiensParDepart(@Query("libelledepartement") String libelleDepartement);
 
     @POST("lesmedcins_par_nom.php")
     Call<ArrayList<Praticien>> lesPraticiensParNom();
