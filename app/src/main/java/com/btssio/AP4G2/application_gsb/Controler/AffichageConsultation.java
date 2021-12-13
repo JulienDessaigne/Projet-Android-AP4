6f9102c7d01d6b68c68f389ec8acc63cc2bd055c
veschembes <mveschembes@gmail.com>
2021-12-13 14:56:27 +0100

    Évolution des Affichage Consultation

--- app/src/main/java/com/btssio/AP4G2/application_gsb/Controler/AffichageConsultation.java
+++ app/src/main/java/com/btssio/AP4G2/application_gsb/Controler/AffichageConsultation.java
@@ -2,7 +2,6 @@ package com.btssio.AP4G2.application_gsb.Controler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,9 +9,12 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
+import com.btssio.AP4G2.application_gsb.Modele.Departement;
+import com.btssio.AP4G2.application_gsb.Modele.DepartementDAOConnecte;
+import com.btssio.AP4G2.application_gsb.Modele.Praticien;
+import com.btssio.AP4G2.application_gsb.Modele.PraticienDAOConnecte;
 import com.btssio.AP4G2.application_gsb.R;
 
 import java.util.ArrayList;
@@ -20,13 +22,13 @@ import java.util.HashMap;
 
 public class AffichageConsultation extends AppCompatActivity {
 
+    private Departement DepartementSelectionne;
     private Spinner spinnerDepartement;
     private ListView listviewEntete;
-    private ListView listviewConcurrent;
+    private ListView listviewPraticien;
 
-    SpinnerAdapter spinner_departement;
     ListAdapter adapter_entete;
-    ListAdapter adapter_concurrent;
+    ListAdapter adapterPraticien;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,82 +37,103 @@ public class AffichageConsultation extends AppCompatActivity {
 
 
         Initialiser();
-        remplirspinnerDepartement();
+        remplirSpinnerDepartementParBouton();
         remplirlistviewEntete();
-        remplirlistviewConcurrent();
-        gestion_suppression();
+        remplirListViewPraticienParBouton(DepartementSelectionne);
+        //gestion_suppression();
     }
 
     public void Initialiser() {
+
+        // Valorisation des variables représentant les éléments XML
+        spinnerDepartement = findViewById(R.id.spinnerDepartement);
         listviewEntete = findViewById(R.id.listViewEntete);
-        listviewConcurrent = findViewById(R.id.listViewMedecin);
+        listviewPraticien = findViewById(R.id.listViewPraticien);
     }
 
-    public void remplirspinnerDepartement() {
-        Log.d("log", "methode remplirSinViticulteurs()");
+    public void remplirSpinnerDepartementParBouton() {
 
-        final ViticulteurDAO viticulteurAcces = new ViticulteurDAO() {
+        // Récupération de l'information de source d'information
+        Bundle extras = getIntent().getExtras();
+        String boutonChoisi = (extras != null) ? extras.getString("consultationType") : "";
 
-            @Override
-            public void onTacheTerminee(final ArrayList<Viticulteur> lesViticulteurs) {
-                Log.d("log", "MainActivity.getViticulteurs : " + lesViticulteurs.toString());
+        // Récupération de l'information en fonction du bouton cliqué
+        if (boutonChoisi.equals("Connecte")) {
 
-                // remplissage du spinner a l'aide de la liste recuperee
-                Spinner spinViticulteurs = findViewById(R.id.spinViticulteurs);
+            // Récupération des données via Retrofit
+            final DepartementDAOConnecte DepartementAcces = new DepartementDAOConnecte() {
 
-                ArrayAdapter<String> spinViticulteursAdapter = new ArrayAdapter<>(
-                        MainActivity.this.getBaseContext(), android.R.layout.simple_spinner_item
-                );
+                @Override
+                public void onTacheTerminee(final ArrayList<Departement> lesDepartements) {
 
-                for (int i = 0; i < lesViticulteurs.size(); i++) {
-                    spinViticulteursAdapter.add(lesViticulteurs.get(i).getNomV());
+                    // Appel de la méthode remplissant le spinner des départements
+                    remplirSpinnerDepartementParListeDepartement(lesDepartements);
                 }
 
-                spinViticulteurs.setAdapter(spinViticulteursAdapter);
-                // gestion des evenements sur le spinner de selection du viticulteur
-                spinViticulteurs.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onTacheTerminee(String resultat) {
+                    // TODO Auto-generated method stub
+                }
 
-                    @Override
-                    public void onItemSelected(AdapterView<?> arg0, View arg1,
-                                               int arg2, long arg3) {
-                        // TODO Auto-generated method stub
-                        Log.d("log", arg2 + " " + lesViticulteurs.get(arg2));
-                        viticulteurSelectionne = lesViticulteurs.get(arg2);
-                    }
+                @Override
+                public void onTacheTerminee(Departement resultat) {
+                    // TODO Auto-generated method stub
+                }
 
-                    @Override
-                    public void onNothingSelected(AdapterView<?> arg0) {
-                        // TODO Auto-generated method stub
-                    }
-                });
+                @Override
+                public void onErreur(String message) {
+                    Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT).show();
+                    // TODO Auto-generated method stub
+                }
+            };
+            DepartementAcces.getDepartementsPraticienDAOConnecte();
+        } else {
+            if (boutonChoisi.equals("Deconnecte")) {
+                // Récupération des données via SQLiteOpenHelper
+            } else {
+                // Valorisation à vide du tableau de recencement,
+                // dans le cas d'un accès à l'affichage sans passage par les boutons disponibles
+                remplirListViewPraticienParListePraticiens(new ArrayList<Praticien>());
+            }
+        }
+    }
+    
+    public void remplirSpinnerDepartementParListeDepartement(final ArrayList<Departement> lesDepartements) {
+        
+        // Déclaration de l'adaptateur pour le spinner
+        ArrayAdapter<String> spinDepartementsAdapter = new ArrayAdapter<>(
+                AffichageConsultation.this.getBaseContext(), android.R.layout.simple_spinner_item
+        );
+
+        // Récupération des noms des départements
+        for (int i = 0; i < lesDepartements.size(); i++) {
+            spinDepartementsAdapter.add(lesDepartements.get(i).getNOM());
+        }
+
+        spinnerDepartement.setAdapter(spinDepartementsAdapter);
+        // gestion des evenements sur le spinner de selection du Departement
+        spinnerDepartement.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                DepartementSelectionne = lesDepartements.get(arg2);
             }
 
             @Override
-            public void onTacheTerminee(String resultat) {
+            public void onNothingSelected(AdapterView<?> arg0) {
                 // TODO Auto-generated method stub
             }
-
-            @Override
-            public void onTacheTerminee(Viticulteur resultat) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onErreur(String message) {
-                Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT).show();
-                // TODO Auto-generated method stub
-            }
-        };
-        viticulteurAcces.getViticulteursDAO();
+        });
     }
 
     public void remplirlistviewEntete() {
 
         // Création d'un tableau associatif
         HashMap<String, String> map_entete = new HashMap<>();
-        map_entete.put("nom", "Nom");
-        map_entete.put("prenom", " Prenom");
-        map_entete.put("note", " Note");
+        map_entete.put("PRA_NOM", "Nom");
+        map_entete.put("PRA_PRENOM", " Prenom");
 
         // Création collection
         ArrayList<HashMap<String, String>> maliste_entete = new ArrayList<>();
@@ -119,79 +142,119 @@ public class AffichageConsultation extends AppCompatActivity {
         try {
 
             adapter_entete = new SimpleAdapter(this,
-                    maliste_entete, R.layout.colonnes, new
-                    String[] { "nom", "prenom", "note" }, new
+                    maliste_entete, R.layout.colonne, new
+                    String[] { "PRA_NOM", "PRA_PRENOM" }, new
                     int[] {
-                    R.id.textViewColonne1, R.id.textViewColonne2, R.id.textViewColonne3
+                    R.id.textViewColonne1, R.id.textViewColonne2
             });
-            // MAJ de la listview à utilisant l'adapter
 
             listviewEntete.setAdapter(adapter_entete);
         } catch (Exception e) {}
     }
 
-    public void remplirlistviewConcurrent() {
+    public void remplirListViewPraticienParBouton(Departement DepartementSelectionne) {
 
-        final Gestionnaire_viticulteur unGestionnaireviticulteur = (Gestionnaire_viticulteur) getApplicationContext();
+        // Récupération de l'information de source d'information
+        Bundle extras = getIntent().getExtras();
+        String boutonChoisi = (extras != null) ? extras.getString("consultationType") : "";
 
-        // Création collection
-        ArrayList<HashMap<String, String>> maliste_concurrent = new ArrayList<>();
+        // Récupération de l'information en fonction du bouton cliqué
+        if (boutonChoisi.equals("Connecte")) {
+            
+            // Récupération des données via Retrofit
+            final PraticienDAOConnecte PraticienAcces = new PraticienDAOConnecte() {
 
-        for (Viticulteur unViticulteur : unGestionnaireviticulteur.getLesViticulteurs()) {
+                @Override
+                public void onTacheTerminee(final ArrayList<Praticien> lesPraticiens) {
+                    
+                    // Utilisation de la liste de Praticiens récupérée
+                    remplirListViewPraticienParListePraticiens(lesPraticiens);
+                }
 
-            if (unViticulteur instanceof Viticulteur_concurrent) {
+                @Override
+                public void onTacheTerminee(String resultat) {
+                    // TODO Auto-generated method stub
+                }
 
-                // Création d'un tableau associatif
-                HashMap<String, String> map_concurrent = new HashMap<>();
+                @Override
+                public void onTacheTerminee(Praticien resultat) {
+                    // TODO Auto-generated method stub
+                }
 
-                // Rajout des valeurs dans le Hashmap
-                map_concurrent.put("nom", unViticulteur.getNomV());
-                map_concurrent.put("prenom", unViticulteur.getPrenomV());
-                map_concurrent.put("note", String.valueOf(((Viticulteur_concurrent) unViticulteur).getNoteV()));
-
-                // Rajout du Hashmap dans la collection
-                maliste_concurrent.add(map_concurrent);
+                @Override
+                public void onErreur(String message) {
+                    Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT).show();
+                    // TODO Auto-generated method stub
+                }
+            };
+            PraticienAcces.getPraticiensParDepartementDAOConnecte(DepartementSelectionne.getNOM());
+        } else {
+            if (boutonChoisi.equals("Deconnecte")) {
+                // Récupération des données via SQLiteOpenHelper
+            } else {
+                // Valorisation à vide du tableau de recencement,
+                // dans le cas d'un accès à l'affichage sans passage par les boutons disponibles
+                remplirListViewPraticienParListePraticiens(new ArrayList<Praticien>());
             }
+        }
+    }
+    
+    public void remplirListViewPraticienParListePraticiens(ArrayList<Praticien> lesPraticiens) {
+        
+        // Création collection
+        ArrayList<HashMap<String, String>> listePraticien = new ArrayList<>();
+
+        for (Praticien unPraticien : lesPraticiens) {
+
+            // Création d'un tableau associatif
+            HashMap<String, String> mapPraticien = new HashMap<>();
+
+            // Rajout des valeurs dans le Hashmap
+            mapPraticien.put("PRA_NOM", unPraticien.getPRA_NOM());
+            mapPraticien.put("PRA_PRENOM", unPraticien.getPRA_PRENOM());
+
+            // Rajout du Hashmap dans la collection
+            listePraticien.add(mapPraticien);
         }
 
         try {
 
-            adapter_concurrent = new SimpleAdapter(this,
-                    maliste_concurrent,
-                    R.layout.colonnes,
-                    new String[] { "nom", "prenom", "note" },
-                    new int[] { R.id.NomC, R.id.PrenomC, R.id.NoteC
+            adapterPraticien = new SimpleAdapter(this,
+                    listePraticien,
+                    R.layout.colonne,
+                    new String[] { "PRA_NOM", "PRA_PRENOM" },
+                    new int[] { R.id.textViewColonne1, R.id.textViewColonne2
                     });
 
             // MAJ de la listview à utilisant l'adapter
-            listviewConcurrent.setAdapter(adapter_concurrent);
+            listviewPraticien.setAdapter(adapterPraticien);
         } catch (Exception e) {}
     }
 
-    public void gestion_suppression(){
-
-        final Gestionnaire_viticulteur unGestionnaireviticulteur = (Gestionnaire_viticulteur) getApplicationContext();
-
-        listviewConcurrent.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                String itemClique = adapterView.getItemAtPosition(i).toString();
-                itemClique = itemClique.substring(1, itemClique.length()-1);
-                String[] keyValuePairs = itemClique.split(",");
-                HashMap<String,String> ItemDecoupe = new HashMap<>();
-
-                for(String pair : keyValuePairs) {
-                    String[] entry = pair.split("=");
-                    ItemDecoupe.put(entry[0].trim(), entry[1].trim());
-                }
-
-                Toast.makeText(getApplicationContext(), "Viticulteur supprimé !", Toast.LENGTH_LONG).show();
-                unGestionnaireviticulteur.supprimerViticulteur(unGestionnaireviticulteur.getViticulteurs(ItemDecoupe.get("nom"), ItemDecoupe.get("prenom")));
-
-                remplirlistviewConcurrent();
-            }
-        });
-    }
+//    public void gestion_suppression(){
+//
+//        final Gestionnaire_Departement unGestionnaireDepartement = (Gestionnaire_Departement) getApplicationContext();
+//
+//        listviewPraticien.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                String itemClique = adapterView.getItemAtPosition(i).toString();
+//                itemClique = itemClique.substring(1, itemClique.length()-1);
+//                String[] keyValuePairs = itemClique.split(",");
+//                HashMap<String,String> ItemDecoupe = new HashMap<>();
+//
+//                for(String pair : keyValuePairs) {
+//                    String[] entry = pair.split("=");
+//                    ItemDecoupe.put(entry[0].trim(), entry[1].trim());
+//                }
+//
+//                Toast.makeText(getApplicationContext(), "Departement supprimé !", Toast.LENGTH_LONG).show();
+//                unGestionnaireDepartement.supprimerDepartement(unGestionnaireDepartement.getDepartements(ItemDecoupe.get("nom"), ItemDecoupe.get("prenom")));
+//
+//                remplirListViewPraticienParListePraticiens();
+//            }
+//        });
+//    }
 }
